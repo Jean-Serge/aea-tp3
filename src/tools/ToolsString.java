d40c34351621866e4172dbbd217291d64ae4f695
Jean-Serge <j.monbailly@orange.Fr>
2015-02-27 11:31:14 +0100

    Ajout d'une fonction permettant de savoir si 2 mots de même taille sont identiques à une lettre près.

---
+++ src/tools/ToolsString.java
@@ -0,0 +1,26 @@
+package tools;
+
+public class ToolsString {
+
+	/**
+	 * Retourne true si a et b sont idéntiques à une lettre près.
+	 * (on suppose que a et b ont la même longueur)
+	 * 
+	 * @param a le 1er mot 
+	 * @param b le 2nd mot
+	 * @return true si a et b n'ont qu'une lettre de différence
+	 */
+	public static boolean diffUneLettre (String a, String b) {
+	    // a et b supposees de meme longueur
+	    int i=0 ;
+	    while (i<a.length () && a.charAt (i) == b.charAt (i))
+	      ++i ;
+	    if (i == a.length ()) return false ;
+	    ++i ;
+	    while (i<a.length () && a.charAt (i) == b.charAt (i))
+	      ++i ;
+	    if (i == a.length ()) return true ;
+	    return false ;
+	  }
+	
+}
