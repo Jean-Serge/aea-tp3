d92465a9b5428ddcd1607fa72404bb58530576cd
Jean-Serge <j.monbailly@orange.Fr>
2015-02-27 10:39:10 +0100

    Correction de l'utilisation d'une liste de String par une liste d'entiers. (utilisation de la classe Liste)

--- src/graphe/Graphe.java
+++ src/graphe/Graphe.java
@@ -1,32 +1,26 @@
 package graphe;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * Classe représentant un graphe dans cet exercice.
- * Composé d'un tableau de mot et d'un tableau de leurs successeurs respectifs.
+ * Classe représentant un graphe dans cet exercice. Composé d'un tableau de mots
+ * et d'un tableau d'indice de leurs successeurs respectifs.
  * 
  * @author monbailly
- *
+ * 
  */
 public class Graphe {
 
 	private String[] mot;
-	private List<String>[] listeSucc;
+	private Liste[] listeSucc;
 	private int nb;
-	
+
 	public Graphe(String[] lesMots) {
 		super();
 		this.mot = lesMots;
 		this.nb = this.mot.length;
-		this.listeSucc = new ArrayList[nb];
-		
-		for(List<String> l : this.listeSucc){
-			l = new ArrayList<String>();
-		}
+		this.listeSucc = new Liste[nb];
+
+		for (Liste liste : this.listeSucc)
+			liste = new Liste();
 	}
-	
-	
 
 }
