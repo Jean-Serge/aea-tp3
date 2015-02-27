f4c2942c8efcbd224044fdd011c679d5e95c1914
Jean-Serge <j.monbailly@orange.Fr>
2015-02-27 10:58:23 +0100

    Modification de la classe Liste pour manipuler des tableaux au lieu d'encapsuler une ArrayList<Integer>.

--- src/graphe/Liste.java
+++ src/graphe/Liste.java
@@ -1,8 +1,5 @@
 package graphe;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Classe permettant de manipuler des listes d'entiers (encapsule une ArrayList).
  * @author monbailly
@@ -10,14 +7,53 @@ import java.util.List;
  */
 public class Liste {
 
-	private List<Integer> liste;
+	private final int BASE_LENGTH = 5;
+	private int[] liste;
+	private int nb;
 	
 	/**
 	 * Permet d'initialiser la liste.
 	 */
 	public Liste(){
 		super();
-		this.liste = new ArrayList<Integer>();
+		this.liste = new int[this.BASE_LENGTH];
+		this.nb = 0;
+	}
+	
+
+	/**
+	 * Permet d'ajouter la valeur indiquée à la liste courante.
+	 * Si le tableau est plein, sa capacité est doublée.
+	 * (on suppose la valeur de val correcte)
+	 * 
+	 * @param val la valeur à ajouter au tableau
+	 * 
+	 */
+	public void ajouter(Integer val){
+		if(nb == liste.length){
+			int[] tmp = new int[this.liste.length * 2];
+			for(int i = 0 ; i < this.liste.length ; i++){
+				tmp[i] = this.liste[i];
+			}
+			this.liste = tmp;
+		}
+			
+		this.liste[nb++] = val;
 	}
 
+	/**
+	 * Affiche le contenu du tableau.
+	 * 
+	 * @return un affichage en console du tableau
+	 */
+	public String toString(){
+		String retour = "[" + this.liste[0];
+		
+		for(int i = 1 ; i < this.nb ; i++){
+			retour += ", " + this.liste[i];
+		}
+		
+		retour += "]";
+		return retour;
+	}
 }
